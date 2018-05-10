5bbe997316b1c1d08e0347227bc108eb471abf9e
Ivan Bonkin <inbonkin@avito.ru>
2018-05-10 16:23:21 +0300

    QA-7767 Add Dijkstra's algorithm implementation

---
+++ graphwalker-core/src/main/java/org/graphwalker/core/algorithm/Dijkstra.java
@@ -0,0 +1,162 @@
+package org.graphwalker.core.algorithm;
+
+import org.graphwalker.core.model.Edge.RuntimeEdge;
+import org.graphwalker.core.model.Element;
+import org.graphwalker.core.model.Model;
+import org.graphwalker.core.model.Path;
+import org.graphwalker.core.model.Vertex.RuntimeVertex;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Ivan Bonkin
+ */
+public class Dijkstra implements Algorithm {
+
+  private final List<RuntimeVertex> nodes;
+  private final List<RuntimeEdge> edges;
+  private Set<RuntimeVertex> settledNodes;
+  private Set<RuntimeVertex> unSettledNodes;
+  private Map<RuntimeVertex, RuntimeVertex> predecessors;
+  private Map<RuntimeVertex, Integer> distance;
+
+  public Dijkstra(Model.RuntimeModel model) {
+    // create a copy of the array so that we can operate on this array
+    this.nodes = new ArrayList<>(model.getVertices());
+    this.edges = new ArrayList<>(model.getEdges());
+  }
+
+  public void execute(RuntimeVertex source) {
+    settledNodes = new HashSet<RuntimeVertex>();
+    unSettledNodes = new HashSet<RuntimeVertex>();
+    distance = new HashMap<RuntimeVertex, Integer>();
+    predecessors = new HashMap<RuntimeVertex, RuntimeVertex>();
+    distance.put(source, 0);
+    unSettledNodes.add(source);
+    while (unSettledNodes.size() > 0) {
+      RuntimeVertex node = getMinimum(unSettledNodes);
+      settledNodes.add(node);
+      unSettledNodes.remove(node);
+      findMinimalDistances(node);
+    }
+  }
+
+  private void findMinimalDistances(RuntimeVertex node) {
+    List<RuntimeVertex> adjacentNodes = getNeighbors(node);
+    for (RuntimeVertex target : adjacentNodes) {
+      if (getShortestDistance(target) > getShortestDistance(node)
+        + getDistance(node, target)) {
+        distance.put(target, getShortestDistance(node)
+          + getDistance(node, target));
+        predecessors.put(target, node);
+        unSettledNodes.add(target);
+      }
+    }
+  }
+
+  private int getDistance(RuntimeVertex node, RuntimeVertex target) {
+    for (RuntimeEdge edge : edges) {
+      if (edge.getSourceVertex().equals(node)
+        && edge.getTargetVertex().equals(target)) {
+        Double weight = edge.getWeight();
+        if (weight < 0) {
+          throw new IllegalStateException("Dijkstra algorithm operates only on non-negative values");
+        }
+        return weight.intValue();
+      }
+    }
+    throw new RuntimeException("Should not happen");
+  }
+
+  private List<RuntimeVertex> getNeighbors(RuntimeVertex node) {
+    List<RuntimeVertex> neighbors = new ArrayList<RuntimeVertex>();
+    for (RuntimeEdge edge : edges) {
+      if (edge.getSourceVertex().equals(node)
+        && !isSettled(edge.getTargetVertex())) {
+        neighbors.add(edge.getTargetVertex());
+      }
+    }
+    return neighbors;
+  }
+
+  private RuntimeVertex getMinimum(Set<RuntimeVertex> vertexes) {
+    RuntimeVertex minimum = null;
+    for (RuntimeVertex vertex : vertexes) {
+      if (minimum == null) {
+        minimum = vertex;
+      } else {
+        if (getShortestDistance(vertex) < getShortestDistance(minimum)) {
+          minimum = vertex;
+        }
+      }
+    }
+    return minimum;
+  }
+
+  private boolean isSettled(RuntimeVertex vertex) {
+    return settledNodes.contains(vertex);
+  }
+
+  private int getShortestDistance(RuntimeVertex destination) {
+    Integer d = distance.get(destination);
+    if (d == null) {
+      return Integer.MAX_VALUE;
+    } else {
+      return d;
+    }
+  }
+
+  /*
+   * This method returns the path from the source to the selected target and
+   * NULL if no path exists
+   */
+  public Path<Element> getPath(RuntimeVertex target) {
+    LinkedList<RuntimeVertex> path = new LinkedList<RuntimeVertex>();
+    RuntimeVertex step = target;
+    // check if a path exists
+    if (predecessors.get(step) == null) {
+      return null;
+    }
+    path.add(step);
+    while (predecessors.get(step) != null) {
+      step = predecessors.get(step);
+      path.add(step);
+    }
+    // Put it into the correct order
+    Collections.reverse(path);
+
+    return withEdges(new Path<>(path));
+  }
+
+  private Path<Element> withEdges(Path<RuntimeVertex> vertices) {
+    Path<Element> elements = new Path<>();
+    if (!vertices.isEmpty()) {
+      elements.add(vertices.getFirst());
+    }
+
+    List<RuntimeVertex> list = new ArrayList<>(vertices);
+    a:
+    for (int i = 1; i < vertices.size(); i++) {
+      for (RuntimeEdge edge : this.edges) {
+        RuntimeVertex source = list.get(i - 1);
+        RuntimeVertex target = list.get(i);
+        if (edge.getSourceVertex().equals(source)
+          && edge.getTargetVertex().equals(target)) {
+          elements.add(edge);
+          elements.add(target);
+          continue a;
+        }
+      }
+      throw new IllegalStateException("No egde between");
+    }
+    return elements;
+  }
+
+}
---
+++ graphwalker-core/src/test/java/org/graphwalker/core/algorithm/DijkstraTest.java
@@ -0,0 +1,47 @@
+package org.graphwalker.core.algorithm;
+
+import org.graphwalker.core.model.Edge;
+import org.graphwalker.core.model.Element;
+import org.graphwalker.core.model.Model;
+import org.graphwalker.core.model.Path;
+import org.graphwalker.core.model.Vertex;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+public class DijkstraTest {
+
+  private static final Vertex v00 = new Vertex().setName("v00").setId("v00");
+  private static final Vertex v01 = new Vertex().setName("v01").setId("v01");
+  private static final Vertex v10 = new Vertex().setName("v10").setId("v10");
+  private static final Vertex v20 = new Vertex().setName("v20").setId("v20");
+  private static final Vertex v31 = new Vertex().setName("v31").setId("v31");
+
+  private static final Edge e1 = new Edge().setName("e1").setId("e1").setSourceVertex(v00).setTargetVertex(v01);
+  private static final Edge e2 = new Edge().setName("e2").setId("e2").setSourceVertex(v00).setTargetVertex(v10);
+  private static final Edge e3 = new Edge().setName("e3").setId("e3").setSourceVertex(v10).setTargetVertex(v20);
+  private static final Edge e4 = new Edge().setName("e4").setId("e4").setSourceVertex(v20).setTargetVertex(v31);
+  private static final Edge e5 = new Edge().setName("e5").setId("e5").setSourceVertex(v01).setTargetVertex(v31);
+
+  private static final Model model = new Model().addEdge(e1).addEdge(e2).addEdge(e3).addEdge(e4).addEdge(e5);
+
+  @Test
+  public void shortestDistance() throws Exception {
+    Dijkstra dijkstra = new Dijkstra(model.build());
+    dijkstra.execute(v00.build());
+
+    Path<Element> path = dijkstra.getPath(v31.build());
+    System.out.println(path);
+    assertThat(path, hasSize(4 + 3));
+  }
+
+  @Test
+  public void noDistance() throws Exception {
+    Dijkstra dijkstra = new Dijkstra(model.build());
+    dijkstra.execute(v00.build());
+    assertThat(dijkstra.getPath(v00.build()), is(equalTo(null)));
+  }
+}
